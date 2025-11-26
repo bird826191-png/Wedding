e2130ef23d60ae9744f2d4201b0775fce2fe44e0
bird826191-png <bird826191@gmail.com>
2025-11-26 15:33:21 +0000

    Web

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative paths for assets on GitHub Pages
+  base: '/Wedding/', // Ensures relative paths for assets on GitHub Pages
   build: {
     outDir: 'dist',
   },
